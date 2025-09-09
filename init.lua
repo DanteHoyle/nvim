2d03953c190a83119e24b1f25c1ca7367d6a4830
Dante Hoyle <hoyledante@gmail.com>
2025-09-09 08:06:21 -0700

    split autocmds to their own file

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@
 -- |  \| |/ _ \/ _ \ \ / / | '_ ` _ \
 -- | |\  |  __/ (_) \ V /| | | | | | |
 -- |_| \_|\___|\___/ \_/ |_|_| |_| |_|
+require 'config.autocmd'
 require 'options'
 require 'keymaps'
 require 'config.lazy'
---
+++ lua/config/autocmd.lua
@@ -0,0 +1,39 @@
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+-- only show cursorline in active window normal mode
+autocmd({ "InsertLeave", "WinEnter", "TabEnter", "TermLeave" }, {
+  group = augroup("SmartCursorline", { clear = false }),
+  desc = "Enable cursorline only in active window",
+  callback = function()
+    vim.wo.cursorline = vim.bo.buftype == ""
+  end,
+})
+autocmd({ "InsertEnter", "WinLeave", "TabLeave" }, {
+  group = augroup("SmartCursorline", { clear = false }),
+  desc = "Enable cursorline only in active window",
+  callback = function()
+    vim.wo.cursorline = false
+  end,
+})
+
+-- Set folding method to LSP if supported
+autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client:supports_method('textDocument/foldingRange') then
+      local win = vim.api.nvim_get_current_win()
+      vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
+    end
+  end,
+})
+
+-- visually show highlight when yanking text
+autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = augroup('YankHLAttach', { clear = true }),
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
