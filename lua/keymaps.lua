f73862f8e2b0de9a3d2438af13d6b2f808e368f8
Dante Hoyle <hoyledante@gmail.com>
2025-11-24 16:17:45 -0800

    updated keymaps to be more strucutred with custom utility funcs

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,37 +1,25 @@
-local util = require 'config.utility'
-
--- normal mode keymap
-local function nmap(keys, func, desc)
-  desc = desc or ''
-  vim.keymap.set('n', keys, func, { desc = desc })
-end
-
--- insert mode keymap
-local function imap(keys, func, desc)
-  desc = desc or ''
-  vim.keymap.set('i', keys, func, { desc = desc })
-end
+local u = require 'config.utility'
 
 -- Clear current highlights with Esc
-nmap('<Esc>', '<cmd>nohlsearch<CR>', 'Clear highlights')
+u.nmap('<Esc>', '<cmd>nohlsearch<CR>', 'Clear highlights')
 
 -- Toggle line wrapping with Leader + w
-nmap('<Leader>W', '<cmd>set wrap!<CR>','Toggle line wrapping')
+u.nmap('<Leader>W', '<cmd>set wrap!<CR>','Toggle line wrapping')
 
 -- Center the screen when moving up or down in a file
-nmap('<C-d>', '<C-d>zz')
-nmap('<C-u>', '<C-u>zz')
-
--- Open current file externally
-nmap('<leader>O', util.open_current_file, 'Open the current file externaly')
-
-nmap('<leader>pi', util.paste_image_from_clipboard, 'Paste currently copied image')
+u.nmap('<C-d>', '<C-d>zz')
+u.nmap('<C-u>', '<C-u>zz')
 
 -- Delete words in insert mode with Alt + Backspace
-imap('<BS>', '<C-W>', 'Delete words with alt + backspace')
+u.imap('<BS>', '<C-W>', 'Delete words with alt + backspace')
 
 -- Add floating popup to diagnostic jumps
-nmap(']d', function() vim.diagnostic.jump({ count = 1, float = true }) end, 'Next diagnostic')
-nmap('[d', function() vim.diagnostic.jump({ count = -1, float = true }) end, 'Prev diagnostic')
+u.nmap(']d', function() vim.diagnostic.jump({ count = 1, float = true }) end, 'Next diagnostic')
+u.nmap('[d', function() vim.diagnostic.jump({ count = -1, float = true }) end, 'Prev diagnostic')
 
-nmap('<leader>id', '<cmd>NoteInsertDate<cr>', 'Insert Date for Notes Heading')
+u.nmap('<leader>id', '<cmd>NoteInsertDate<cr>', 'Insert Date for Notes Heading')
+
+-- Open current file externally
+u.nmap('<leader>O', '<cmd>OpenCurrentFile<cr>', 'Open the current file externaly')
+
+u.nmap('<leader>pi', '<cmd>NoteInsertCopiedImage<cr>', 'Paste currently copied image')
